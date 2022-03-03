d61643d0a20131c45de939edde88c78a717ef785
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-03 15:11:52 +0530

    changed

--- scripts/veracodelib.py
+++ scripts/veracodelib.py
@@ -1,9 +1,9 @@
 import os
 import xml.etree.ElementTree as ET
-buildinfo = open("buildinfo.xml", "r")
+buildinfo = open("output.xml", "r")
 build_info="<?xml"+buildinfo.read().split("<?xml")[-1]
 mytree = ET.ElementTree(ET.fromstring(build_info))
 root = mytree.getroot()
 
-os.system('python $GITHUB_WORKSPACE/scripts/buildinfo_xml_extract.py')
+os.system('python $GITHUB_WORKSPACE/scripts/veracodelib.py')
 print(root.attrib['build_id'])
