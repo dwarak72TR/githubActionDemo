a7735bebf59668ac993f33f2d4d27e4bb082c9eb
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-07 15:04:08 +0530

    pushing new changes

--- scripts/veracodelib.py
+++ scripts/veracodelib.py
@@ -1,6 +1,6 @@
 
 def get_buildID():
-    filereader = open("$GITHUB_WORKSPACE/output.xml", "r")
+    filereader = open("output.xml", "r")
     readxmlfile = filereader.read()
     result = readxmlfile.split("build_id=",2) #readxml[231:239]
     buildid = (result[1])[1:9]
