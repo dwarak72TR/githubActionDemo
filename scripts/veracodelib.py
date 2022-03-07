c4effcf7880fb75fc8354de13ca7137d158683f3
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-07 15:10:42 +0530

    vhanges

--- scripts/veracodelib.py
+++ scripts/veracodelib.py
@@ -1,8 +1,15 @@
 
+from matplotlib.pyplot import get
+
+
 def get_buildID():
     filereader = open("output.xml", "r")
     readxmlfile = filereader.read()
     result = readxmlfile.split("build_id=",2) #readxml[231:239]
     buildid = (result[1])[1:9]
-    print(buildid)
+    return buildid
+
+
+build_id = get_buildID()
+print(build_id)
 
