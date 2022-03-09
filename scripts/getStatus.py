810035d5a33b6bb9ef81e6d7611dcb6ba6c594cd
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-09 22:27:24 +0530

    pushing

--- scripts/getStatus.py
+++ scripts/getStatus.py
@@ -4,7 +4,8 @@ def get_status():
     result = readxmlfile.split("published_date_sec=",2) #readxml[231:239]
     result2 = result[1]
     status = result2.split("status=",2)
-    return status[1].split("/>",2)
+    finalcheck = status[1].split("/",2)
+    return finalcheck[0]
 
 
 status = get_status()
