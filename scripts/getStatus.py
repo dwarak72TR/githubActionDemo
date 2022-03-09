daf110587171a6f9c6fa1a619615e9a64d9d56b9
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-09 22:22:52 +0530

    pushing

--- scripts/getStatus.py
+++ scripts/getStatus.py
@@ -2,8 +2,9 @@ def get_status():
     filereader = open("output.xml", "r")
     readxmlfile = filereader.read()
     result = readxmlfile.split("published_date_sec=",2) #readxml[231:239]
-    result2 = result[1].split("status=",2)
-    return result2
+    result2 = result[1]
+    status = result2.split("status=",2)
+    return status[0]
 
 
 status = get_status()
