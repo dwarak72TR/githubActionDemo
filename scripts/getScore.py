d21b65eb29fb287a9d5a566e23d33e03b2965507
N Ch <dwarakesh.nallanichakravarthula@thomsonreuters.com>
2022-03-12 22:42:25 +0530

    first commit int hi branch

--- scripts/getScore.py
+++
@@ -1,9 +0,0 @@
-def get_score():
-    filereader = open("summaryreport.xml", "r")
-    readxmlfile = filereader.read()
-    result = readxmlfile.split("score=",2) #readxml[231:239]
-    score = (result[1])[1:3]
-    return score
-
-score = get_score()
-print(score)
